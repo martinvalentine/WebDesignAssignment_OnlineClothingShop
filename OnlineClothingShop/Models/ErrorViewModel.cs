51323f16159d006209ea69c0aaec12a5abcf8e4a
Martin Valentine <nguythequang200@gmail.com>
2023-10-22 01:11:48 +0700

    Init project

---
+++ OnlineClothingShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineClothingShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
