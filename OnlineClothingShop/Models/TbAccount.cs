e39c4cb60ffc339131cc8778520fc3be0bd64d0b
Martin Valetine <nguythequang200@gmail.com>
2023-10-25 16:45:21 +0700

    Recreate models from database using DBContext

---
+++ OnlineClothingShop/Models/TbAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineClothingShop.Models
+{
+    public partial class TbAccount
+    {
+        public int AccountId { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
+        public bool? Active { get; set; }
+        public string? FullName { get; set; }
+        public int? RoleId { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        public virtual TbRole? Role { get; set; }
+    }
+}
---
+++ OnlineClothingShop/Models/TbCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineClothingShop.Models
+{
+    public partial class TbCategory
+    {
+        public TbCategory()
+        {
+            TbProducts = new HashSet<TbProduct>();
+        }
+
+        public int CatId { get; set; }
+        public string? CatName { get; set; }
+        public string? Thumb { get; set; }
+
+        public virtual ICollection<TbProduct> TbProducts { get; set; }
+    }
+}
