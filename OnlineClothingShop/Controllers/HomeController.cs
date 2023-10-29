2337b4e2d2fb1b7843b437f99d8b2414762deee4
Martin Valetine <nguythequang200@gmail.com>
2023-10-29 21:48:59 +0700

    #36 Add Filter by categories feature and fix some bug

--- OnlineClothingShop/Controllers/HomeController.cs
+++ OnlineClothingShop/Controllers/HomeController.cs
@@ -6,29 +6,22 @@ namespace OnlineClothingShop.Controllers
 {
     public class HomeController : Controller
     {
-
         private OnlineClothingShopContext db;
 
-		public HomeController(OnlineClothingShopContext db)
-		{
-			this.db = db;
-		}
+        public HomeController(OnlineClothingShopContext db)
+        {
+            this.db = db;
+        }
 
-		public IActionResult Index()
+        public IActionResult Index()
         {
             var product = db.TbProducts.ToList();
             return View(product);
         }
-            
+
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ OnlineClothingShop/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineClothingShop.Models;
+
+namespace OnlineClothingShop.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private OnlineClothingShopContext db;
+
+
+        public ProductController(OnlineClothingShopContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Shop()
+        {
+            var product = db.TbProducts.ToList();
+            return View(product);
+        }
+        public IActionResult Detail(int? id)
+        {
+            var product = db.TbProducts.Where(p => p.ProductId == id).FirstOrDefault();
+            return View(product);
+        }
+
+        public IActionResult CategoryFilter(int? catid)
+        {
+            if (catid == null)
+            {
+                var products = db.TbProducts.ToList();
+                return PartialView("Filter", products);
+            }
+            else
+            {
+                var products = db.TbProducts.Where(l => l.CatId == catid).Include(m => m.Cat).ToList();
+                return PartialView("Filter", products);
+            }
+        }
+    }
+}
